675423fd500adae18ffd67a3086379f4f07dfc4b
InDaviX <dszewczyk235@gmail.com>
2023-12-07 18:51:52 +0100

    Wyjmowanie z arrayów i obiektów

--- play.js
+++ play.js
@@ -1,13 +1,24 @@
-const hobbies = ["sports","cooking"];
-const copied = [...hobbies, ...["gaming"]];
-console.log(copied);
+const person = {name: "Dawid", age: 18, work: "studying"};
+// __Tak byśmy to zrobili normalnie:__
+// const name = person.name;
+// const age = person.age;
+// const work = person.work;
 
-const person = {name: "Dawid", age: 18};
-const copied2 = {...person, ...{hobby: "gaming"}};
-console.log(copied2);
+// const {name, work} = person;
+// console.log(name, work);
 
-// const toArray = (...args) => args;
-function toArray(...args){
-    return args;
-}
-console.log(toArray(2,3,4).reduce((total, current) => {return total + current;}));
+const numbers = [1,2,3,4,5];
+// __Tak byśmy to zrobili normalnie:__
+// const first = numbers[0]
+// const second = numbers[1]
+// const third = numbers[2]
+// const fourth = numbers[3]
+// const fifth = numbers[4]
+
+const [first, second, third] = numbers;
+console.log(first, second, third);
+
+
+// __Tak można wybrać elementy POZA JAKIMIŚ Z NICH:__
+// const {work, ...rest} = person;
+// console.log(work, rest);
