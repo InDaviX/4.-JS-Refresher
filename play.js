8b9ab58142067f15d9c20afb634df69acb91eff2
InDaviX <dszewczyk235@gmail.com>
2023-12-07 18:37:30 +0100

    Tablice i ciekawe funkcje na nich

--- play.js
+++ play.js
@@ -1,8 +1,17 @@
-const person = {
-    name: 'John',
-    age: 34,
-    greet: function (){
-        console.log("Hi, I'm " + this.name + "!");
-    }
-}
-console.log(person.greet());
+let hobbies = ["sports","cooking", 25, true, {name: "baking"}, ["readings","traveling"]]
+
+for (let hobby of hobbies) {
+    console.log(hobby);
+};
+
+for (let hobby in hobbies) {
+    console.log(hobbies[hobby]);
+};
+
+let hobbierArray = ["sports","cooking","painting"];
+let hobbiesSubset = hobbierArray.slice(1,3);
+let fillteredHobbies = hobbierArray.filter((val) => val !== "sports");
+console.log(fillteredHobbies);
+
+let updatedHobbies = hobbierArray.map((val) => "hobby: "+val);
+console.log(updatedHobbies);
