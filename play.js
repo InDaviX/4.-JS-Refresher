1088a92066f3bb269c8c9a1389bf773805d0248d
InDaviX <dszewczyk235@gmail.com>
2023-12-07 18:38:30 +0100

    Referencje (oszukiwanie constant values)

--- play.js
+++ play.js
@@ -1,17 +1,10 @@
-let hobbies = ["sports","cooking", 25, true, {name: "baking"}, ["readings","traveling"]]
+const hobbies = ["reading","cooking"];
+hobbies.push("programming");
+console.log(hobbies);
 
-for (let hobby of hobbies) {
-    console.log(hobby);
-};
-
-for (let hobby in hobbies) {
-    console.log(hobbies[hobby]);
-};
-
-let hobbierArray = ["sports","cooking","painting"];
-let hobbiesSubset = hobbierArray.slice(1,3);
-let fillteredHobbies = hobbierArray.filter((val) => val !== "sports");
-console.log(fillteredHobbies);
-
-let updatedHobbies = hobbierArray.map((val) => "hobby: "+val);
-console.log(updatedHobbies);
+const hobbiesObject = {
+    name: "Dawid",
+    type: "Programming"
+}
+hobbiesObject.age = 18;
+console.log(hobbiesObject);
